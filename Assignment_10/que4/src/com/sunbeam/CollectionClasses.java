12c531a20fd4be7624131dca14af07bb35185f94
mahendra-deshmukh <mahendra.deshmukh@hotmail.com>
2023-10-19 14:33:09 +0530

    Asign 10 Updated

--- Assignment_10/que4/src/com/sunbeam/CollectionClasses.java
+++ Assignment_10/que4/src/com/sunbeam/CollectionClasses.java
@@ -6,8 +6,8 @@ public class CollectionClasses {
 
 	public static void main(String[] args) {
 		Collection<String> c = new ArrayList<>(); //null allowed with duplicates
-		//Collection<String> c = new HashSet<>(); //null allowed but no duplicates, replaces
-		//Collection<String> c = new LinkedHashSet<>();//null allowed but no duplicates, replaces
+		//Collection<String> c = new HashSet<>(); //null allowed but no duplicates
+		//Collection<String> c = new LinkedHashSet<>();//null allowed but no duplicates
 		//Collection<String> c = new TreeSet<>();// does not allow nulls
 		c.add("B");
 		c.add("D");
